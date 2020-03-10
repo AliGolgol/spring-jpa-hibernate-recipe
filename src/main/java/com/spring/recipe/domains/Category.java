0ef10e3bc8bb4343f4b666268b888096b22ee8d0
aligolgol <golgol.postbox@gmail.com>
2020-03-10 16:30:00 +0330

    Creating domains. Closes #1

---
+++ src/main/java/com/spring/recipe/domains/Category.java
@@ -0,0 +1,21 @@
+package com.spring.recipe.domains;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Data
+@EqualsAndHashCode(exclude = {"recipe"})
+@Entity
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String description;
+
+    @ManyToMany(mappedBy = "categories")
+    private Set<Recipe> recipes;
+}
